be2903bcded68b534a820b44b36819377dc5d02d
Darcy Linde <lindedarcy@gmail.com>
2019-02-17 17:13:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,6 +25,9 @@ WIN_COMBINATIONS = [
       position_1 = board[win_index_1]
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return combo
+        els
       end
     end
   end
