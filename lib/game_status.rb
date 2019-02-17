e48be3f989c8b9188a06cf83cf6dd6a981a685c4
Darcy Linde <lindedarcy@gmail.com>
2019-02-17 16:45:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,38 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Diagonal top-bottom
+  [6,4,2]  # Diagonal bottom-top
+  ]
+  
+  #won?
+  def won?(board)
+    
+  end
+  
+  #full?
+  def full?(board)
+    
+  end
+  
+  #draw?
+  def draw?(board)
+    
+  end
+  
+  #over?
+  def over?(board)
+    
+  end
+  
+  #winner
+  def winner(board)
+    
+  end
